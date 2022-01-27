c917ce0250dd238568e68b7db9affafe7ebe6ce9
dache <dasha-ufo@rambler.ru>
2022-01-27 22:06:10 +0300

    Initial commit

---
+++ src/tasks/FirstTask.java
@@ -0,0 +1,7 @@
+package tasks;
+
+public class FirstTask {
+ public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/tasks/SecondTask.java
@@ -0,0 +1,8 @@
+package tasks;
+
+public class SecondTask {
+    public static void main(String[] args) {
+        double j = 144.55;
+        byte a = 127;
+    }
+}
